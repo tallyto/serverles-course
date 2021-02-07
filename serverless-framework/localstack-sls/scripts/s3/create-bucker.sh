0c0c1bba6a0200aeda1330f77eefbac01fdf985c
otallyto <rodrigues.tallyto@hotmail.com>
2021-02-06 21:34:58 -0400

    create localstack-sls

---
+++ serverless-framework/localstack-sls/scripts/s3/create-bucker.sh
@@ -0,0 +1,2 @@
+aws --endpoint-url=http://localhost:4566 s3 mb s3://balde
+aws --endpoint-url=http://localhost:4566 s3 ls
---
+++ serverless-framework/localstack-sls/scripts/s3/upload-file.sh
@@ -0,0 +1,3 @@
+
+aws --endpoint-url=http://localhost:4566 s3 cp test.txt s3://balde
+aws --endpoint-url=http://localhost:4566 s3 ls s3://balde
