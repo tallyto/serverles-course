3f572332fdd6586d96540c08f046e98f674509e2
otallyto <rodrigues.tallyto@hotmail.com>
2021-02-08 20:26:58 -0400

    finish sqs listener

--- serverless-framework/sqs-s3-streams/src/sqs.listener.js
+++ serverless-framework/sqs-s3-streams/src/sqs.listener.js
@@ -1,20 +1,29 @@
+const AWS = require('aws-sdk')
 class Hander {
-  async main (event) {
-    console.log("event**", JSON.stringify(event, null, 2))
+  async main(event) {
+    const [{ body, messageId }] = event.Records;
+    const item = JSON.parse(body);
+    console.log(
+      "**event",
+      JSON.stringify(
+        { ...item, messageId, at: new Date().toISOString() },
+        null,
+        2
+      )
+    );
     try {
       return {
         statusCode: 200,
-        body: 'Hello'
-      }
+        body: "Hello",
+      };
     } catch (error) {
-      console.log("deu ruim**", error.stack)
+      console.log("deu ruim**", error.stack);
       return {
         statusCode: 500,
-        body: 'Internal server error!!'
-      }
+        body: "Internal server error!!",
+      };
     }
   }
 }
-
-const hander = new Hander()
-module.exports = hander.main.bind(hander)
+const hander = new Hander();
+module.exports = hander.main.bind(hander);
