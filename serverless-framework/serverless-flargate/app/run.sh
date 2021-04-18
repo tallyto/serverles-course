ffed9d0ce5b613f51bfc2e592001f6e95083026d
otallyto <rodrigues.tallyto@hotmail.com>
2021-04-18 19:37:00 -0300

    update script

--- serverless-framework/serverless-flargate/app/run.sh
+++ serverless-framework/serverless-flargate/app/run.sh
@@ -1,5 +1,5 @@
 IMAGE_URL="149239330696.dkr.ecr.us-east-1.amazonaws.com/process-data"
 
-docker build -t $IMAGE_URL .
+docker build -t 149239330696.dkr.ecr.us-east-1.amazonaws.com/process-data .
 
-docker run $IMAGE_URL
+docker run 149239330696.dkr.ecr.us-east-1.amazonaws.com/process-data
